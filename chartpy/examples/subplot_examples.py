2b20e793b153d9c29a5d41a141c98e624d9362cc
saeedamen <saeedamen@hotmail.com>
2016-08-19 18:10:49 +0100

    Added subplot for matplotlib and bokeh

--- chartpy/examples/subplot_examples.py
+++ chartpy/examples/subplot_examples.py
@@ -11,7 +11,8 @@ from chartpy import Chart, Style
 quandl_api_key = "x"
 
 # choose run_example = 0 for everything
-# run_example = 1 - plot US GDP QoQ (real) and nominal with Plotly with subplots for each line
+# run_example = 1 - plot US GDP QoQ (real) and nominal with Plotly/Bokeh/Matplotlib with subplots for each line
+# run_example = 2 - plot US GDP QoQ (real + nominal) in two double plots (passing an array of dataframes)
 run_example = 0
 
 if run_example == 1 or run_example == 0:
@@ -24,4 +25,23 @@ if run_example == 1 or run_example == 0:
     # Chart object is initialised with the dataframe and our chart style
     chart = Chart(df=df, chart_type='line', style=style)
 
+    chart.plot(engine='matplotlib')
+    chart.plot(engine='bokeh')
     chart.plot(engine='plotly')
+
+if run_example == 2 or run_example == 0:
+    df = Quandl.get(["FRED/A191RL1Q225SBEA", "FRED/A191RP1Q027SBEA"], authtoken=quandl_api_key)
+    df.columns = ["Real QoQ", "Nominal QoQ"]
+
+    df = [df, df]
+
+    # set the style of the plot
+    style = Style(title="US GDP double plot", source="Quandl/Fred", subplots=True)
+
+    # Chart object is initialised with the dataframe and our chart style
+    chart = Chart(df=df, chart_type='line', style=style)
+
+    chart.plot(engine='bokeh')
+    chart.plot(engine='matplotlib')
+    # chart.plot(engine='plotly') TODO!
+

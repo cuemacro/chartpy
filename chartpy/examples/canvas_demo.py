a330c0552c7f90ae49ef31f9a51001d4252498a7
saeedamen <saeedamen@hotmail.com>
2016-10-14 23:36:07 +0100

    Changed dashboard locat

--- chartpy/examples/canvas_demo.py
+++ chartpy/examples/canvas_demo.py
@@ -28,13 +28,13 @@ if run_example == 1 or run_example == 0:
 
     # Chart object is initialised with the dataframe and our chart style
     chart_bokeh = Chart(df=df, chart_type='line', engine='bokeh',
-                        style=Style(title="US GDP", source="Quandl/Fred", scale_factor=-1, width=500, height=300, silent_display=True))
+                        style=Style(title="US GDP", source="Quandl/Fred", scale_factor=-2, width=500, height=300, silent_display=True))
 
     chart_plotly = Chart(df=df, chart_type='line', engine='plotly',
-                         style=Style(title="US GDP", source="Quandl/Fred", scale_factor=-1, width=500, height=300, silent_display=True))
+                         style=Style(title="US GDP", source="Quandl/Fred", scale_factor=-2, width=500, height=300, silent_display=True))
 
     chart_matplotlib = Chart(df=df, chart_type='line', engine='matplotlib',
-                             style=Style(title="US GDP", source="Quandl/Fred", scale_factor=-1, width=500, height=300, silent_display=True))
+                             style=Style(title="US GDP", source="Quandl/Fred", scale_factor=-2, width=500, height=300, silent_display=True))
 
     text = "A demo of chartpy canvas!!"
 
